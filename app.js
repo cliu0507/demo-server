89969ae55409a9276c73281a4fa2e101f64c67b5
cliu <cliu0507@yahoo.com>
2020-11-01 14:51:58 -0800

    add a new API for detection

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const { assert } = require('console');
 
 // set variables for message queue
 const queueName = 'demo-queue'
+const queueName2 = 'domo-detect'
 
 // set variables for mongodb client
 const MongoClient = mongodb.MongoClient;
@@ -130,6 +131,164 @@ app.post('/upload', upload.single('myfile'), function(req, res){
 
                 // make msg for message queue
                 var queue = queueName
+                var msg = {
+                    'jobId': jobId,
+                    'filePath': filePath,
+                    'destination': destination,
+                    'mimeType':mimeType,
+                    'originalName':originalName
+                    }
+                client.close()
+                logger.log({
+                    level:"info",
+                    message: "Success: connection to DB closed"
+                    })
+                
+                //connect to message queue
+                amqp.connect('amqp://localhost', function(error0, connection){
+                    if (error0){
+                        throw error0
+                    }
+                    logger.log({
+                        level:'info',
+                        message: 'Success: connection to queue built!'
+                    })
+                    connection.createChannel(function(error1,channel){
+                        if (error1){
+                            throw error1
+                        }
+                        logger.log({
+                            level:'info',
+                            message: 'Success: connection to channel built!'
+                        })
+                        
+                        var fullpath = __dirname + '/' + req.file.path  //get absolute path of file
+                        msg['filePath'] = fullpath // update msg object with absolute path
+                        channel.assertQueue(queue, {
+                            durable: false
+                        });
+                        
+                        // send to mq
+                        channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)));
+                        logger.log({
+                            level:'info',
+                            message: 'Success: job message sent to MQ!'
+                        });
+                        logger.log({
+                            level:'info',
+                            message: 'Success: message is \''.concat(msg.toString()).concat('\'')
+                        });
+
+                    })
+                    setTimeout(function() {
+                        logger.log({
+                            level:'info',
+                            message: 'Success: connection to queue closed!'
+                        })
+                        connection.close();
+                        //process.exit(0);
+                        logger.log({
+                            level:'info',
+                            message: 'Success: ----- post call completed! -----'
+                        })
+                    }, 100);
+
+                    });       
+                            
+                });
+
+            });
+        });
+    //console.log('Body- ' + JSON.stringify(req.body));
+    //send response
+    res.setHeader("Content-Type","application/json");
+    res.send(jobId)
+});
+
+
+app.post('/domo', upload.single('myfile'), function(req, res){
+    logger.log({
+        level:'info',
+        message: 'Success: ----- multipart post call received! -----'
+    })
+
+    //set variables for this job
+    var filePath = req.file.path
+    var destination = req.file.destination
+    var mimeType = req.file.mimetype
+    var originalName = req.file.originalname
+
+    //make database record
+    var jobId = uuidv1()
+
+    //connect to database
+    MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client){
+        if (err){
+            logger.log({
+                level:"info",
+                message: "Error: connection to MongoDB failed!"
+            })
+            throw err;
+        }
+        var dbo = client.db(dbName)
+        var record = {
+            "jobId": jobId,
+            "jobStatus": 'created',
+            "queueName": queueName2,
+            "filePath": filePath,
+            "mimeType": mimeType,
+            "originalName": originalName
+        }
+
+        dbo.collection(collectionName).insertOne(record, function(err,res){
+            if(err){
+                logger.log({
+                    level:"info",
+                    message: "Error: insert to DB failed!"
+                })
+            }
+            logger.log({
+                level:"info",
+                message: "Success: new job created on DB!"
+            })    
+        })
+
+        var bucket = new mongodb.GridFSBucket(dbo)
+        var newFileName = jobId + '_file'
+        var stream = bucket.openUploadStream(newFileName)
+        var fileId = stream.id // file object id on gridfs
+
+        // upload file to GridFS
+        fs.createReadStream(filePath).pipe(stream).
+        on('error', function(error){
+            assert.ifError(error)
+        }).
+        on('finish', function(){
+            logger.log({
+                level:"info",
+                message: "Success: file uploaded to GridFS"
+            })
+            
+            // update record to database
+            record.newFileName = newFileName
+            record.fileId = fileId.toString()
+            record.jobStatus = 'uploaded'
+            var query = {jobId: jobId}
+            var newRecord = { $set: record };
+            dbo.collection(collectionName).updateOne(query, newRecord, function(err,res){
+                if(err){
+                    logger.log({
+                        level:"info",
+                        message: "Error: update to DB failed!"
+                    })
+                }
+                logger.log({
+                    level:"info",
+                    message: "Success: update job status to 'uploaded' on DB!"
+                });
+
+                // make msg for message queue
+                var queue = queueName2
                 var msg = {
                     'jobId': jobId,
                     'filePath': filePath,
